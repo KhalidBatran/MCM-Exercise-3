6cec8c3169d0e9fd7864fc9bacf74b526a1901e5
KhalidBatran <khalood.159@live.com>
2024-09-09 20:34:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ df = pd.read_csv("https://raw.githubusercontent.com/KhalidBatran/MCM-Exercise-3/
 
 image_path = 'assets/logo-mmu.png'
 
+# Define the color map for each continent to maintain consistency
+color_map = {"Asia": "#636EFA", "Africa": "#EF553B", "America": "#00CC96", "Europe": "#AB63FA", "Oceania": "#FFA15A"}
+
 app.layout = [html.H1('MCM7183 Exercise 3'), 
               html.Img(src=image_path), 
               dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='dropdown-country'),
@@ -36,12 +39,16 @@ def update_graph(country_selected, year_selected):
     subset_Year_America = subset_Year[subset_Year['state'].isin(["America"])]
     subset_Year_Europe = subset_Year[subset_Year['state'].isin(["Europe"])]
     subset_Year_Oceania = subset_Year[subset_Year['state'].isin(["Oceania"])]
+    
     pie_data = [sum(subset_Year_Asia['gdp']), sum(subset_Year_Africa['gdp']),
                 sum(subset_Year_America['gdp']), sum(subset_Year_Europe['gdp']),
                 sum(subset_Year_Oceania['gdp'])]
+    
     mylabels = ["Asia", "Africa", "America", "Europe", "Oceania"]
+    
     pie_df = {'Continent': mylabels, 'GDP': pie_data}
-    fig2 = px.pie(pie_df, values="GDP", names="Continent")
+    fig2 = px.pie(pie_df, values="GDP", names="Continent", 
+                  color="Continent", color_discrete_map=color_map)
 
     return fig, fig2
 
