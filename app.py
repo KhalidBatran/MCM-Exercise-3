d62eb1e33e6c80d550f1caf758887a83ca9b5362
KhalidBatran <khalood.159@live.com>
2024-09-09 20:23:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ app.layout = [html.H1('MCM7183 Exercise 3'),
               html.Img(src=image_path), 
               dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='dropdown-country'),
               dcc.Graph(id="graph-scatter"), 
-              dcc.Slider(min=2000, max=2020, step=10, value=2020, id='slider-year', marks={2000: '2000', 2010: '2010', 2020: '2020'}),
+              dcc.Slider(min=1960, max=2020, step=5, value=2020, id='slider-year',
+                         marks={1960: '1960', 1970: '1970', 1980: '1980', 1990: '1990', 
+                                2000: '2000', 2010: '2010', 2020: '2020'}),
               dcc.Graph(id="graph-pie")]
 
 @callback(
@@ -34,10 +36,12 @@ def update_graph(country_selected, year_selected):
     subset_Year_America = subset_Year[subset_Year['state'].isin(["America"])]
     subset_Year_Europe = subset_Year[subset_Year['state'].isin(["Europe"])]
     subset_Year_Oceania = subset_Year[subset_Year['state'].isin(["Oceania"])]
-    pie_data = [sum(subset_Year_Asia['gdp']),sum(subset_Year_Africa['gdp']),sum(subset_Year_America['gdp']),sum(subset_Year_Europe['gdp']),sum(subset_Year_Oceania['gdp'])]
-    mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
-    pie_df = {'Continent': mylabels,'GDP': pie_data}
-    fig2 = px.pie(pie_df,values="GDP",names="Continent")
+    pie_data = [sum(subset_Year_Asia['gdp']), sum(subset_Year_Africa['gdp']),
+                sum(subset_Year_America['gdp']), sum(subset_Year_Europe['gdp']),
+                sum(subset_Year_Oceania['gdp'])]
+    mylabels = ["Asia", "Africa", "America", "Europe", "Oceania"]
+    pie_df = {'Continent': mylabels, 'GDP': pie_data}
+    fig2 = px.pie(pie_df, values="GDP", names="Continent")
 
     return fig, fig2
 
