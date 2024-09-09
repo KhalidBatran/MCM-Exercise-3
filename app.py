9ed69ccb0657956263cae11443dae244065a64fb
KhalidBatran <khalood.159@live.com>
2024-09-09 20:13:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,20 @@ server = app.server
 
 df = pd.read_csv("https://raw.githubusercontent.com/KhalidBatran/MCM-Exercise-3/main/assets/gdp_1960_2020.csv")
 
-
-
 image_path = 'assets/logo-mmu.png'
 
 app.layout = [html.H1('MCM7183 Exercise 3'), 
               html.Img(src=image_path), 
               dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='dropdown-country'),
               dcc.Graph(id="graph-scatter"), 
-              dcc.Dropdown([{'label':'2020', 'value':2020}, {'label':'2010', 'value':2010}, 
-                            {'label':'2000', 'value':2000}], 2020, id='dropdown-year'),
+              dcc.Slider(min=2000, max=2020, step=10, value=2020, id='slider-year', marks={2000: '2000', 2010: '2010', 2020: '2020'}),
               dcc.Graph(id="graph-pie")]
 
 @callback(
     Output('graph-scatter', 'figure'),
     Output('graph-pie', 'figure'),
     Input('dropdown-country', 'value'),
-    Input('dropdown-year', 'value'),
+    Input('slider-year', 'value'),
 )
 def update_graph(country_selected, year_selected):
     subset_Country = df[df['country'].isin([country_selected])]
@@ -37,7 +34,7 @@ def update_graph(country_selected, year_selected):
     subset_Year_America = subset_Year[subset_Year['state'].isin(["America"])]
     subset_Year_Europe = subset_Year[subset_Year['state'].isin(["Europe"])]
     subset_Year_Oceania = subset_Year[subset_Year['state'].isin(["Oceania"])]
-    pie_data = [sum(subset_Year_Asia['gdp']),sum(subset_Year_Africa['gdp']),sum(subset_Year_America['gdp']),sum(subset_Year_Europe['gdp']),sum(subset_Year_Oceania['gdp'])];
+    pie_data = [sum(subset_Year_Asia['gdp']),sum(subset_Year_Africa['gdp']),sum(subset_Year_America['gdp']),sum(subset_Year_Europe['gdp']),sum(subset_Year_Oceania['gdp'])]
     mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
     pie_df = {'Continent': mylabels,'GDP': pie_data}
     fig2 = px.pie(pie_df,values="GDP",names="Continent")
