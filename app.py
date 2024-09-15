4839499ad8a42a3c97a9eb96ad805d19a41e3d8f
KhalidBatran <khalood.159@live.com>
2024-09-16 07:37:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 server = app.server
 
+# Load the updated dataset
 df = pd.read_csv('https://raw.githubusercontent.com/KhalidBatran/MCM-Exercise-3/main/assets/cleaned_medals.csv')
 
 app.layout = dbc.Container([
@@ -15,7 +16,7 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dcc.Dropdown(id='filter-country', 
-                         options=[{'label': country, 'value': country} for country in df['Country_Code'].unique()],
+                         options=[{'label': country, 'value': country} for country in df['Country Code'].unique()],
                          value=[], 
                          multi=True, 
                          placeholder="Select Country(s)")
@@ -29,7 +30,7 @@ app.layout = dbc.Container([
         ], width=4),
         dbc.Col([
             dcc.Dropdown(id='filter-medal', 
-                         options=[{'label': medal, 'value': medal} for medal in df['Medal_Type'].unique()],
+                         options=[{'label': medal, 'value': medal} for medal in df['Medal Type'].unique()],
                          value=[], 
                          multi=True, 
                          placeholder="Select Medal Type(s)")
@@ -50,17 +51,17 @@ def update_graph(selected_countries, selected_gender, selected_medals):
     filtered_df = df.copy()
 
     if selected_countries:
-        filtered_df = filtered_df[filtered_df['Country_Code'].isin(selected_countries)]
+        filtered_df = filtered_df[filtered_df['Country Code'].isin(selected_countries)]
     if selected_gender != 'All':
         filtered_df = filtered_df[filtered_df['Gender'] == selected_gender]
     if selected_medals:
-        filtered_df = filtered_df[filtered_df['Medal_Type'].isin(selected_medals)]
+        filtered_df = filtered_df[filtered_df['Medal Type'].isin(selected_medals)]
     
     fig = go.Figure(
         data=[
-            go.Bar(name='Gold', x=filtered_df['Country_Code'], y=filtered_df['Gold'], marker_color='gold'),
-            go.Bar(name='Silver', x=filtered_df['Country_Code'], y=filtered_df['Silver'], marker_color='silver'),
-            go.Bar(name='Bronze', x=filtered_df['Country_Code'], y=filtered_df['Bronze'], marker_color='brown')
+            go.Bar(name='Gold', x=filtered_df['Country Code'], y=filtered_df['Medal Type'].apply(lambda x: x == 'Gold').sum(), marker_color='gold'),
+            go.Bar(name='Silver', x=filtered_df['Country Code'], y=filtered_df['Medal Type'].apply(lambda x: x == 'Silver').sum(), marker_color='silver'),
+            go.Bar(name='Bronze', x=filtered_df['Country Code'], y=filtered_df['Medal Type'].apply(lambda x: x == 'Bronze').sum(), marker_color='brown')
         ]
     )
     
