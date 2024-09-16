a5a70f442f626f9dd77d8cc3c7a58533add1143f
KhalidBatran <khalood.159@live.com>
2024-09-17 07:54:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app.layout = html.Div([
     html.H1('Olympic Medals Count by Country', style={'textAlign': 'center'}),
     dcc.Dropdown(
         id='dropdown-country',
-        options=[{'label': i, 'value': i} for i in df['Country Code'].unique()],
-        value=df['Country Code'].unique(),  # Default to all countries
+        options=[{'label': 'All', 'value': 'All'}] + [{'label': i, 'value': i} for i in df['Country Code'].unique()],
+        value='All',  # Default to 'All'
         multi=True,  # Allow multiple selections
         clearable=False
     ),
@@ -27,29 +27,17 @@ app.layout = html.Div([
     Input('dropdown-country', 'value')
 )
 def update_graph(selected_countries):
-    if not selected_countries:
-        # If no countries are selected, display data for all countries
+    if 'All' in selected_countries or not selected_countries:
         filtered_df = df
     else:
-        # Filter the dataframe for selected countries
         filtered_df = df[df['Country Code'].isin(selected_countries)]
-
-    # Count the medals by type and prepare data for plotting
-    medal_counts = filtered_df.groupby(['Medal Type']).size().reset_index(name='Count')
-    medal_counts = medal_counts.sort_values('Medal Type', ascending=False)  # Sort to maintain color consistency
-
-    # Define custom colors for medal types
-    colors = {'Bronze Medal': 'red', 'Silver Medal': 'blue', 'Gold Medal': 'green'}
-
-    # Create the figure
-    fig = px.bar(
-        medal_counts, 
-        x='Medal Type', 
-        y='Count', 
-        color='Medal Type',
-        color_discrete_map=colors,
-        title='Medals Distribution'
-    )
+    
+    medal_counts = filtered_df.groupby(['Country Code', 'Medal Type']).size().reset_index(name='Count')
+    fig = px.bar(medal_counts, x='Country Code', y='Count', color='Medal Type',
+                 title='Medals Distribution', barmode='group',
+                 color_discrete_map={'Gold Medal': 'green', 'Silver Medal': 'blue', 'Bronze Medal': 'red'})
+    fig.update_layout(legend_title_text='Medal Type',
+                      legend=dict(traceorder='reversed', title_font_family='Arial'))
     return fig
 
 if __name__ == '__main__':
